18897fc48a736232b2f385882542de2b74e8b3c8
Justin R. Miller <incanus@codesorcery.net>
2012-10-10 17:27:26 -0700

    nicer interactivity & exclusive lock on animation

--- MapBox Example/InteractiveLayerViewController.m
+++ MapBox Example/InteractiveLayerViewController.m
@@ -37,47 +37,70 @@
 
 - (void)singleTapOnMap:(RMMapView *)mapView at:(CGPoint)point
 {
-    id source = mapView.tileSource;
-    
-    if ([source conformsToProtocol:@protocol(RMInteractiveSource)] && [(id <RMInteractiveSource>)source supportsInteractivity])
+    if ([self.view.subviews count] == 1)
     {
-        source = (id <RMInteractiveSource>)source;
-        
-        NSString *formattedOutput = [source formattedOutputOfType:RMInteractiveSourceOutputTypeFull 
-                                                         forPoint:point 
-                                                        inMapView:mapView];
-        
-        if ( ! formattedOutput || ! [formattedOutput length])
-            formattedOutput = [source formattedOutputOfType:RMInteractiveSourceOutputTypeTeaser 
-                                                   forPoint:point 
-                                                  inMapView:mapView];
-        
-        if (formattedOutput && [formattedOutput length])
+        id source = mapView.tileSource;
+
+        if ([source conformsToProtocol:@protocol(RMInteractiveSource)] && [(id <RMInteractiveSource>)source supportsInteractivity])
         {
-            CGRect frame = CGRectMake(mapView.frame.size.width - 200, 0, 200, 140);
-            
-            UIWebView *webView = [[UIWebView alloc] initWithFrame:frame];
-            
-            [webView loadHTMLString:formattedOutput baseURL:nil];
-            
-            webView.layer.borderColor = [[UIColor blackColor] CGColor];
-            webView.layer.borderWidth = 2.0;
-            
-            webView.userInteractionEnabled = NO;
-            
-            [self.view addSubview:webView];
-            
-            [UIView animateWithDuration:1.0
-                                  delay:0.5
-                                options:UIViewAnimationCurveEaseOut
-                             animations:^(void)
-                             {
-                                 webView.alpha = 0.0;
-                             }
-                             completion:^(BOOL finished)
-                             {
-                                 [webView removeFromSuperview];
-                             }];
+            source = (id <RMInteractiveSource>)source;
+
+            NSString *formattedOutput = [source formattedOutputOfType:RMInteractiveSourceOutputTypeFull 
+                                                             forPoint:point 
+                                                            inMapView:mapView];
+
+            if ( ! formattedOutput || ! [formattedOutput length])
+                formattedOutput = [source formattedOutputOfType:RMInteractiveSourceOutputTypeTeaser 
+                                                       forPoint:point 
+                                                      inMapView:mapView];
+
+            if (formattedOutput && [formattedOutput length])
+            {
+                mapView.userInteractionEnabled = NO;
+
+                CGRect frame = CGRectMake(0, 0, 200, 140);
+
+                UIWebView *webView = [[UIWebView alloc] initWithFrame:frame];
+
+                [webView loadHTMLString:formattedOutput baseURL:nil];
+
+                webView.layer.borderColor = [[UIColor blackColor] CGColor];
+                webView.layer.borderWidth = 2.0;
+
+                webView.userInteractionEnabled = NO;
+
+                [self.view addSubview:webView];
+
+                webView.transform = CGAffineTransformMakeScale(0.01, 0.01);
+                webView.center    = point;
+                webView.alpha     = 0.0;
+
+                [UIView animateWithDuration:0.5
+                                 animations:^(void)
+                                 {
+                                     webView.transform = CGAffineTransformIdentity;
+                                     webView.center    = mapView.center;
+                                     webView.alpha     = 1.0;
+                                 }
+                                 completion:^(BOOL finished)
+                                 {
+                                     [UIView animateWithDuration:0.5
+                                                           delay:2.0
+                                                         options:UIViewAnimationCurveEaseInOut
+                                                      animations:^(void)
+                                                      {
+                                                          webView.transform = CGAffineTransformMakeScale(0.01, 0.01);
+                                                          webView.center    = point;
+                                                          webView.alpha     = 0.0;
+                                                      }
+                                                      completion:^(BOOL finished)
+                                                      {
+                                                          [webView removeFromSuperview];
+
+                                                          mapView.userInteractionEnabled = YES;
+                                                      }];
+                                 }];
+            }
         }
     }
 }
